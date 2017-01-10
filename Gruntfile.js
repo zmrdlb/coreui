0d426f1654692d684e707071ca65efb9d3511860
zhangmingrui <592044573@qq.com>
2017-01-10 18:47:11 +0800

    修改打包压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
 			coreui: {
 				expand: true,
 				cwd: '../coreui/',
-				src: ['js/widget/lib/*'],
+				src: ['js/widget/lib/**'],
 				dest: '../dist/coreui/'
 			}
 		},
